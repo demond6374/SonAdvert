f361e3776047d648948fa4358c32ef19867f12fc
demond6374 <demond.roberson@gmail.com>
2024-09-23 20:23:18 -0500

    Initial

--- SonAdvert.Frontend/obj/Debug/net8.0/SonAdvert.Frontend.AssemblyInfo.cs
+++ SonAdvert.Frontend/obj/Debug/net8.0/SonAdvert.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SonAdvert.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ca6fb9ae74bb3153e928beb32761861ba68c87c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aff3ae56e3c507cf160b4da184b7209132d43e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SonAdvert.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SonAdvert.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
